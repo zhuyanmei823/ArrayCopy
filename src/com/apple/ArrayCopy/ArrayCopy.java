96a8868d7d3676706304aedc5a46df1d6246186e
zym <1411029255@qq.com>
2019-09-30 17:32:38 +0800

    create a ArrayCopy class

---
+++ src/com/apple/ArrayCopy/ArrayCopy.java
@@ -0,0 +1,10 @@
+﻿public class ArrayCopy{
+	public static void main(String[] args){
+		int c[]= {1,2,3,4,5},d[];
+		d=new int[5];
+		System.arraycopy(c, 0, d, 0, 5); 
+	for(int n=0;n<d.length;n++){
+		System.out.println(d[n]);
+	}
+	}
+}
